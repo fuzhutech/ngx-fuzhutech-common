f81542ee40ca8260b85a334e1e67b51b9edbe6f3
fuzhutech <fuzhutech@163.com>
2018-04-24 19:39:18 +0800

    修改BUG

--- src/app/showcase/fz-demo-message/fz-demo-message.component.ts
+++ src/app/showcase/fz-demo-message/fz-demo-message.component.ts
@@ -16,6 +16,11 @@ export class FzDemoMessageComponent implements OnInit {
 
     createBasicMessage() {
         this._message.info('这是一条普通的提醒');
+
+        this._message.error('这个一条错误提醒');
+        this._message.loading('这是一条加载提醒');
+        this._message.success('这是一条成功提醒');
+        this._message.warning('这是一条警告提醒');
     }
 
 }
